7ddba293b3bebc95123faba207d97406a02584ac
zhaojunfei <773152@qq.com>
2017-08-10 10:00:01 +0800

    https://github.com/zhaojunfei/hap.git

---
+++ src/org/jasig/cas/util/PasswordHelper.java
@@ -0,0 +1,35 @@
+package org.jasig.cas.util;
+
+import com.thinkgem.jeesite.common.security.Digests;
+import com.thinkgem.jeesite.common.utils.Encodes;
+
+public class PasswordHelper {
+
+	public static String entryptPassword(String plainPassword) {
+		String plain = Encodes.unescapeHtml(plainPassword);
+		byte[] salt = Digests.generateSalt(SALT_SIZE);
+		byte[] hashPassword = Digests.sha1(plain.getBytes(), salt, HASH_INTERATIONS);
+		return Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword);
+	}
+	
+	public static void main(String[] args){
+		String password = entryptPassword("admin");
+		System.out.println(password);
+		if(validatePassword("admin", password)){
+			System.out.println("验证成功");
+		}
+	}
+	
+	/**
+	 * 验证密码
+	 * @param plainPassword 明文密码
+	 * @param password 密文密码
+	 * @return 验证成功返回true
+	 */
+	public static boolean validatePassword(String plainPassword, String password) {
+		String plain = Encodes.unescapeHtml(plainPassword);
+		byte[] salt = Encodes.decodeHex(password.substring(0,16));
+		byte[] hashPassword = Digests.sha1(plain.getBytes(), salt, HASH_INTERATIONS);
+		return password.equals(Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword));
+	}
+}
